2c50409b2d812286a483711e5829573e3bbb3706
Jarckrios <jarckrios14@gmail.com>
2026-07-01 15:11:02 -0500

    Conectar frontend con backend online

--- Frontend/services/api.js
+++ Frontend/services/api.js
@@ -1,5 +1,4 @@
-const API_URL = "http://localhost:3000/api";
-
+const API_URL = "https://midespensavirtualbackend.onrender.com/api";
 async function registrarUsuario(datos) {
     try {
         const respuesta = await fetch(`${API_URL}/auth/registro`, {
